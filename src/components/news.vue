af234315c32cccd8491dd902e4b16d419d2a59bb
liushuhao <liushuhao@liushuhaodeMacBook-Pro.local>
2020-02-17 23:30:33 +0800

    疫情新闻

--- src/components/news.vue
+++ src/components/news.vue
@@ -1,23 +1,38 @@
 
 
 <template>
-    <div class="news">
-        <p>
-            asdasds
-        </p>
-    </div>
+  <div class="news">
+    <p>asdasds</p>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      newlist: []
+    };
+  },
+  methods: {
+    getnewsdata() {
+      let url = "http://api.tianapi.com/txapi/ncov/index";
+      let key = "32f470c12df2c779659ab942ac4ee75c";
+      this.$axios.get(url, {
+        params: { key: key }
+      }).then(data=>{
+          console.log(data);
+          
+      });
+    }
+  },
+  created(){
+      this.getnewsdata()
+  }
+};
 </script>
 
 <style lang="scss">
-.news{
-    margin-top: 20px;
-    
+.news {
+  margin-top: 20px;
 }
-    
 </style>
